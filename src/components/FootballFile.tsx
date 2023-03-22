e0f8ca27b20af9553d9905e685589b3483d36807
Beka Chaduneli <120742353+bekaChaduneli@users.noreply.github.com>
2023-03-23 03:15:35 +0400

    Add styles

--- src/components/FootballFile.tsx
+++ src/components/FootballFile.tsx
@@ -3,13 +3,30 @@ import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
 import { Football } from "./Football";
 
+const Box = styled.div`
+  width: 40vw;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Center = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default function FootballFile() {
   return (
-    <Canvas>
-      <Stage environment="city" intensity={0.6}>
-        <Football />
-      </Stage>
-      <OrbitControls enableZoom={false} />
-    </Canvas>
+    <Center>
+      <Box>
+        <Canvas>
+          <Stage environment="city" intensity={0.6}>
+            <Football />
+          </Stage>
+          <OrbitControls enableZoom={false} />
+        </Canvas>
+      </Box>
+    </Center>
   );
 }
